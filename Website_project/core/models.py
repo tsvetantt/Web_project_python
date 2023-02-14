51afa0fdafbf560e5949debdb875a708bcc91344
tsvetantt <cvetan_toshev@abv.bg>
2023-02-14 19:02:59 +0200

    MALKO NO OT SURCE

--- Website_project/core/models.py
+++ Website_project/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = pass
+    id_user = pass
+    bio = pass
+    profile_img = models.ImageField()
+    location = pass
+
