c50ed83f1fb76ac6352501b32adaf2e08c7824fc
tsvetantt <cvetan_toshev@abv.bg>
2023-02-14 23:48:54 +0200

    Login + Signup + Logout

--- Website_project/core/models.py
+++ Website_project/core/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
 
 class Profile(models.Model):
-    user = pass
-    id_user = pass
-    bio = pass
-    profile_img = models.ImageField()
-    location = pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank = True)
+    profile_img = models.ImageField(upload_to='profile_images', default='blank-profile-picture-973460_1280.png')
+    location = models.CharField(max_length=100, blank = True)
+
+    def __str__(self):
+        return self.user.username
 
--- Website_project/core/views.py
+++ Website_project/core/views.py
@@ -1,6 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+from .models import Profile
 # Create your views here.
+
+
+@login_required(login_url='signin')
 
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already exists!')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username is already taken!')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
+        else:
+            messages.info(request, 'Passwords are not macthing!')
+            return redirect('signup')
+
+        
+        
+    else: 
+        return render(request, 'signup.html')
+    
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Invalid personal information')
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
+    
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
+
+
