0261985c94244b87d908ec041de9e162f6530d27
tsvetantt <cvetan_toshev@abv.bg>
2023-02-16 16:25:39 +0200

    made personal feed

--- Website_project/core/views.py
+++ Website_project/core/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-
+from itertools import chain
 from .models import Profile, Post, LikePost, FollowersCount
 # Create your views here.
 
@@ -13,8 +13,21 @@ def index(request):
     user_object = User.objects.get(username=request.user.username) #we get the curent user
     user_profile = Profile.objects.get(user=user_object) #we get his profil
 
+    user_following_list = []
+    feed = []
+
+    user_following = FollowersCount.objects.filter(follower=request.user.username)
+
+    for users in user_following:
+        user_following_list.append(users.user)
+
+    for usernames in user_following_list:
+        feed_lists = Post.objects.filter(user=usernames)
+        feed.append(feed_lists)
+
+    feed_list = list(chain(*feed)) 
     posts = Post.objects.all()
-    return render(request, 'index.html', {'user_profile': user_profile, 'posts': posts})
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts': feed_list})
 
 @login_required(login_url='signin')
 def upload(request):
